c0d66c3038229ab38442ace45cf01c6fc88aa7b7
Camila Norbis <camilanorbis@gmail.com>
2023-04-22 22:54:46 -0300

    code tide up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,14 @@ import data.friend.Friend;
 import handler.BirthdayHandler;
 import handler.FriendsHandler;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
 
-        //Create database table and flat file with friend's birthdays
-        /*ProcessBuilder processBuilder = new ProcessBuilder("load_data.sh");
-        try {
-            processBuilder.start();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }*/
-        
         //reading provided file
         FriendsHandler friendsHandler = new FriendsHandler();
-        friendsHandler.readFile("C:\\Dev\\PersonalProjects\\birthday-reminder\\src\\main\\resources\\friendsBirthdays.txt");
+        friendsHandler.readFile("..\\birthday-reminder\\src\\main\\resources\\friendsBirthdays.txt");
 
         //Populating friends array
         BirthdayHandler birthdayHandler = new BirthdayHandler();
